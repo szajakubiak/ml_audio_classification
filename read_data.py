fdea10c4387afa5e441ef5eec2b172f4a2a721c1
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2023-08-15 13:34:45 +0200

    Select port automatically

--- read_data.py
+++ read_data.py
@@ -1,12 +1,31 @@
 import serial
 from time import sleep
 
-port = "COM10"
 baudrate = 115200
 
 p = serial.Serial(port, baudrate, timeout=1)
 
+def find_port():
+    available_ports = list(serial.tools.list_ports.comports())
+
+    for port in available_ports:
+        p = serial.Serial(port.device, baudrate, timeout=1)
+        p.write("i".encode())
+        retry = 20
+        while retry:
+            if p.in_waiting:
+                out = p.readline().decode().rstrip()
+            else:
+                sleep(0.01)
+                retry-=1
+        p.close()
+        if out == "arduino":
+            return port.device
+
 def get_enviro():
+    port = find_port()
+    p = serial.Serial(port, baudrate, timeout=1)
+
     p.write("e".encode())
 
     retry = 20
@@ -20,6 +39,9 @@ def get_enviro():
     return None
 
 def get_sound(start_delay = 0):
+    port = find_port()
+    p = serial.Serial(port, baudrate, timeout=1)
+
     sleep(start_delay)
     p.write("s".encode())
     
@@ -31,6 +53,7 @@ def get_sound(start_delay = 0):
         else:
             sleep(0.05)
             retry-=1
+    p.close()
     if len(out) > 0:
         return out
     else:
